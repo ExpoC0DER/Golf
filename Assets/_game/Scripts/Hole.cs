023cac575e909a9b0bcb1909aaae645bc001f343
David Kacvinsky <davidkacvinsky@proton.me>
2024-04-04 23:25:02 +0200

    New materials and models

--- Assets/_game/Scripts/Hole.cs
+++ Assets/_game/Scripts/Hole.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace _game.Scripts
@@ -9,13 +11,19 @@ namespace _game.Scripts
         [field: SerializeField] public Transform StartLocation { get; private set; }
         [field: SerializeField] public int PlayerId { get; set; }
         [SerializeField] private Renderer _renderer;
+        [SerializeField] private MeshRenderer[] _renderers;
         private readonly int _materialColorReference = Shader.PropertyToID("_Color");
 
         public void SetGrassColor(Color color)
         {
             MaterialPropertyBlock mpb = new MaterialPropertyBlock();
             mpb.SetColor(_materialColorReference, color);
-            _renderer.SetPropertyBlock(mpb);
+            //_renderer.SetPropertyBlock(mpb);
+            _renderers = GetComponentsInChildren<MeshRenderer>(); 
+            foreach (MeshRenderer r in _renderers)
+            {
+                r.SetPropertyBlock(mpb);
+            }
         }
     }
 }
